9ecc1842c986afbfc93c2d7cdcd55166483cc739
Stefan Gajić <stefan.gajic@nenasal.com>
2022-05-04 12:00:01 +0200

    Basic HTML structure and styling - building the rooms section, creating layout, adding functionality and building its responsiveness

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,8 +5,26 @@
 const heroNavMenu = document.querySelector('#hero-nav-menu');
 const heroNavBurger = document.querySelector('#hero-nav-btn-burger');
 
+const roomsPrevBtn = document.querySelector('#rooms-arr-left');
+const roomsNextBtn = document.querySelector('#rooms-arr-right');
+const roomsSlider = document.querySelector('#rooms-slider');
+const roomsSliderCards = document.querySelectorAll('.rooms-slider-card');
+
 // Event listeners
 
 heroNavBurger.addEventListener('click', () => {
     heroNavMenu.classList.toggle('active');
+});
+
+roomsSliderCards.forEach((sliderCard) => {
+    let cardDimensions = sliderCard.getBoundingClientRect();
+    let cardWidth = cardDimensions.width + 30;
+
+    roomsNextBtn.addEventListener('click', () => {
+        roomsSlider.scrollLeft += cardWidth;
+    });
+
+    roomsPrevBtn.addEventListener('click', () => {
+        roomsSlider.scrollLeft -= cardWidth;
+    });
 });
