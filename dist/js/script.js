6cd35561e9f1a6fb9140ff2cbfd8672c89e7d3e6
Stefan Gajić <stefan.gajic@nenasal.com>
2022-05-04 22:15:27 +0200

    Adding functionality on incrementing and decrementing numbers...

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,6 +5,18 @@
 const heroNavMenu = document.querySelector('#hero-nav-menu');
 const heroNavBurger = document.querySelector('#hero-nav-btn-burger');
 
+const adultsDecrementBtn = document.querySelector('.adults-decrement');
+const adultsIncrementBtn = document.querySelector('.adults-increment');
+const adultsNumber = document.querySelector('.adults-number');
+
+let currentAdultsNumber = Number(adultsNumber.textContent);
+
+const kidsDecrementBtn = document.querySelector('.kids-decrement');
+const kidsIncrementBtn = document.querySelector('.kids-increment');
+const kidsNumber = document.querySelector('.kids-number');
+
+let currentKidsNumber = Number(kidsNumber.textContent);
+
 const roomsPrevBtn = document.querySelector('#rooms-arr-left');
 const roomsNextBtn = document.querySelector('#rooms-arr-right');
 const roomsSlider = document.querySelector('#rooms-slider');
@@ -26,6 +38,42 @@ heroNavBurger.addEventListener('click', () => {
     heroNavMenu.classList.toggle('active');
 });
 
+adultsDecrementBtn.addEventListener('click', () => {
+    if (currentAdultsNumber > 0) {
+        currentAdultsNumber--;
+        adultsNumber.textContent = currentAdultsNumber;
+    } else {
+        adultsNumber.textContent = 0;
+    };
+});
+
+adultsIncrementBtn.addEventListener('click', () => {
+    if (currentAdultsNumber >= 0 && currentAdultsNumber < 5) {
+        currentAdultsNumber++;
+        adultsNumber.textContent = currentAdultsNumber;
+    } else if (currentAdultsNumber === 5) {
+        adultsNumber.textContent = 5;
+    };
+});
+
+kidsDecrementBtn.addEventListener('click', () => {
+    if (currentKidsNumber > 0) {
+        currentKidsNumber--;
+        kidsNumber.textContent = currentKidsNumber;
+    } else {
+        kidsNumber.textContent = 0;
+    };
+});
+
+kidsIncrementBtn.addEventListener('click', () => {
+    if (currentKidsNumber >= 0 && currentKidsNumber < 5) {
+        currentKidsNumber++;
+        kidsNumber.textContent = currentKidsNumber;
+    } else if (currentKidsNumber === 5) {
+        kidsNumber.textContent = 5;
+    };
+});
+
 roomsSliderCards.forEach((sliderCard) => {
     let cardDimensions = sliderCard.getBoundingClientRect();
     let cardWidth = cardDimensions.width + 30;
@@ -41,7 +89,7 @@ roomsSliderCards.forEach((sliderCard) => {
 
 testimonialsSliderCards.forEach((sliderCard) => {
     let cardDimensions = sliderCard.getBoundingClientRect();
-    let cardWidth = cardDimensions.width + 33;
+    let cardWidth = cardDimensions.width + 34;
 
     testimonialsNextBtn.addEventListener('click', () => {
         testimonialsSlider.scrollLeft += cardWidth;
