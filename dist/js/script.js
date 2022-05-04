39a9d57033d720f40a1ff0d3f11a0c554db3d1ea
Stefan Gajić <stefan.gajic@nenasal.com>
2022-05-04 14:30:31 +0200

    Basic HTML structure and styling - building the blog section, creating layout, adding functionality and building its responsiveness

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,6 +10,11 @@ const roomsNextBtn = document.querySelector('#rooms-arr-right');
 const roomsSlider = document.querySelector('#rooms-slider');
 const roomsSliderCards = document.querySelectorAll('.rooms-slider-card');
 
+const blogPrevBtn = document.querySelector('#blog-arr-left');
+const blogNextBtn = document.querySelector('#blog-arr-right');
+const blogSlider = document.querySelector('#blog-slider');
+const blogSliderCards = document.querySelectorAll('.blog-slider-card');
+
 // Event listeners
 
 heroNavBurger.addEventListener('click', () => {
@@ -26,5 +31,18 @@ roomsSliderCards.forEach((sliderCard) => {
 
     roomsPrevBtn.addEventListener('click', () => {
         roomsSlider.scrollLeft -= cardWidth;
+    });
+});
+
+blogSliderCards.forEach((sliderCard) => {
+    let cardDimensions = sliderCard.getBoundingClientRect();
+    let cardWidth = cardDimensions.width + 30;
+
+    blogNextBtn.addEventListener('click', () => {
+        blogSlider.scrollLeft += cardWidth;
+    });
+
+    blogPrevBtn.addEventListener('click', () => {
+        blogSlider.scrollLeft -= cardWidth;
     });
 });
